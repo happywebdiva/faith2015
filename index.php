e6571fec4fd974d01b9106c56798dd5f6c982210
happywebdiva <git@happywebdiva.com>
2014-08-30 10:40:08 -0500

    Adjusting home page columns
    To make layout closer to golden ratio

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package faith2015
+ */
+?>
+<?php get_header(); ?>
+<div class="sixteen columns" id="main">
+  <?php 
+		if( !is_front_page() ) {
+	  	if ( function_exists('yoast_breadcrumb') ) {yoast_breadcrumb('<p id="breadcrumbs">','</p>');} 
+		}
+	?>
+  <div class="eleven columns alpha" role="main">
+  <?php 
+    if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+      get_template_part( 'content', get_post_format() );
+      endwhile; 
+      a11yall_paging_nav();  
+    else : 
+      get_template_part( 'content', 'none' );
+    endif; ?>
+  </div><!--.twelve.columns-->
+	<div class="five columns omega">
+    <?php get_sidebar(); ?>
+  </div><!--.four.columns-->
+</div><!--.sixteen.columns-->
+<?php get_footer(); ?>
