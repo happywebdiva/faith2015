7e79dd88b675bc0b3dff0dec969eafb96a720670
happywebdiva <git@happywebdiva.com>
2014-08-31 19:42:07 -0500

    Adding Education & Ministry templates

--- functions.php
+++ functions.php
@@ -8,7 +8,21 @@ function faith2015_widgets_init() {
 		'id' => 'about-widgets',
 		'description' => __( 'Drag to create or update the sidebar for pages using the "About Us Template"'),
 		'before_widget' => '<li class="custom-widget">',
-		'after_widget' => "</li>",
+		'after_widget' => "</div>",
+	) );
+	register_sidebar( array(
+		'name' => 'Education Sidebar',
+		'id' => 'educ-widgets',
+		'description' => __( 'Drag to create or update the sidebar for pages using the Education Template'),
+		'before_widget' => '<li class="custom-widget">',
+		'after_widget' => "</div>",
+	) );
+	register_sidebar( array(
+		'name' => 'Ministry Sidebar',
+		'id' => 'ministry-widgets',
+		'description' => __( 'Drag to create or update the sidebar for pages using the Ministry Template'),
+		'before_widget' => '<li class="custom-widget">',
+		'after_widget' => "</div>",
 	) );
 	register_sidebar( array(
 		'name' => 'Footer Widget',
---
+++ page-education.php
@@ -0,0 +1,41 @@
+<?php
+/* Template Name: Education Template
+ * @package faith2015
+ */
+?>
+<?php get_header(); ?>
+<div class="sixteen columns" id="main">
+  <?php 
+		if( !is_front_page() ) {
+	  	if ( function_exists('yoast_breadcrumb') ) {yoast_breadcrumb('<p id="breadcrumbs">','</p>');} 
+		}
+	?>
+  <div class="eleven columns alpha" role="main">
+	<?php 
+    while ( have_posts() ) : the_post();
+    get_template_part( 'content', 'page' );
+    endwhile;
+		if ( comments_open() || '0' != get_comments_number() ) :
+			comments_template();
+		endif;
+  ?>
+  </div><!--.eleven.columns-->
+	<div class="five columns omega">
+  <?php // get_sidebar('page'); ?>
+    <div class="widget-area" role="complementary">
+      <h2 class="visuallyhidden">Sidebar</h2>
+      <?php if ( !dynamic_sidebar( 'Page Sidebar' ) ) : ?>
+        <aside id="archives" class="widget">
+          <ul>
+					<?php 
+            if ( !dynamic_sidebar('Education Sidebar') ) : 
+              wp_list_pages('title_li=&depth=2&sort_column=menu_order');
+            endif; 
+          ?>
+          </ul>
+        </aside>
+      <?php endif; ?>
+    </div><!-- .widget-area -->
+  </div><!--.five.columns-->
+</div><!--.sixteen.columns-->
+<?php get_footer(); ?>
